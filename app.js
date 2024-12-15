2e13cffa5d9777c009c63eafdc5d2213458b8fe0
Sahithi1101 <sahithi1510@gmail.com>
2024-12-15 01:42:17 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 let cells = document.querySelectorAll(`.cell`)
 cells = Array.from(cells)
+
+let currentPlayer = "X"
+
+let winnercomb = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7],
+]
