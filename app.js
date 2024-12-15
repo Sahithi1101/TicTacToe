f39a5a1698adf293c486468a15085bea9098a2e1
Sahithi1101 <sahithi1510@gmail.com>
2024-12-15 16:01:10 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-let headerText = document.getElementById(`.Title`)
-let cells = document.querySelectorAll(`.cell`)
-const restartbtn =document.getElementById(`.restartbtn`)
+let headerText = document.getElementById("Title");
+let cells = document.getElementsByClassName("cell");
+cells = Array.from(cells);
+const restartbtn = document.getElementById("restartbtn");
+// console.log(cells)
 
 const player1 = "x"
 const player2 = "O"
-let currentPlayer = "X"
+let currentPlayer = player1
+let spaces = Array("","","","","","","","","")
+// console.log(spaces)
 
-let winnercomb = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
-]
