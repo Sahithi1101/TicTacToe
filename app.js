6521c1acbaa3aaf086f17de552d6a304bb9575be
Sahithi1101 <sahithi1510@gmail.com>
2024-12-15 23:51:35 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,75 @@
 let headerText = document.getElementById("Title");
 let cells = document.getElementsByClassName("cell");
 cells = Array.from(cells);
-const restartBtn = document.getElementById("restartbtn");
-let winText = document.getElementById("winmessage")
+let restartBtn = document.getElementById("restartbtn");
+let winText = document.getElementById("winmessage");
+let winner = getComputedStyle(document.body).getPropertyValue('--winningCells')
 // console.log(cells)
 
 const player1 = "x"
 const player2 = "O"
 let currentPlayer = player1
-let spaces = Array("","","","","","","","","")
+let spaces = Array(9).fill(null)
 // console.log(spaces)
 
-const startGame = ()=> {
+const startGame = () => {
     cells.forEach(cell => cell.addEventListener('click', cellClicked))
 }
 
-function cellClicked(cellclick) {
-    const id = cellclick.target.id
+function cellClicked(a) {
+    const id = a.target.id
    
     if (!spaces[id]){
-        spaces[id] = currentPlayer
-        cellclick.target.innerText = currentPlayer
+         spaces[id] = currentPlayer
+        a.target.innerText = currentPlayer
 
-        if(playerWon()){
-            winText = `${currentPlayer} has won`
+        if(playerWon()  !==false){
+            winText.innerHTML= `${currentPlayer} has won`;
+        // } else if (spaces.every(space => space !==null)){
+        //     winText.innerHTML = "It's a tie";
+        }
+            let winnigCells = playerWon()
+
+            winnigCells.map(cell =>cells[cell].computedStyleMap.backgroundColor=winner)
+
+            console.log(winnigCells);
         }
 
-        if (currentPlayer === player1) {
-            currentPlayer = player2; 
-         } else{
-            currentPlayer = player1
-         }
-    }
+        currentPlayer = currentPlayer == player1 ? player2 : player1
 }
-restartBtn.addEventListener('click', restart)
+
+
+const winnerCond = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7],
+]
+
+function playerWon() {
+    for (const condition of winnerCond) {
+        let [a,b,c] = condition;
+
+        if(spaces[a] && spaces[a] === spaces[b] && spaces[a] === spaces[c]){
+            return [a,b,c]
+        }
+    }
+    return false
+}
+
+restartBtn.addEventListener('click', restart);
 
 function restart() {
-    spaces.fill(null)
+    spaces.fill(null);
 
     cells.forEach( cell => {
-        cell.innerText = ''
-    })
+        cell.innerText = '';
+    });
+    headerText = 'TIC TAC TOE'
     currentPlayer = player1
 }
 
